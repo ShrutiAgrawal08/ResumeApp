04f8634510d127da261505cda558dd6c9e9714f7
ShrutiAgrawal08 <shruti.agrawal_mca18@gla.ac.in>
2019-01-13 12:33:18 -0800

    add image and add texteditor

---
+++ app/src/main/java/shru/resumeapp/MainActivity.java
@@ -0,0 +1,13 @@
+package shru.resumeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+j
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
